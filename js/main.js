c28a18d3eb9d034adfc7c58fc2be6380479be224
Alon Dai <alondai1@gmail.com>
2019-12-16 13:26:58 +0200

    removed barak's name from the app

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let gStudentNames = ["Alina Boshkov", "Alon Tal", "Arkadi Zvalinov", "Ben Levi", "Ben Yager", "Coral Solomon", "Daniil Glezer", "Edgar", "Eyal Ganor", "Gilad Bergmann", "Igor", "Ilya Levin", "Lior BenHaim", "Mai Aloni", "Margad T", "Matt Gordon", "Mor Zeevi", "Nevo Kotlovsky", "Niv Leibovitch", "Ohad Avidar", "Paolo Groppi", "Roy Amar", "Shaked Katsir", "shay rosenthal", "Shmuel Elkis", "Tal Azenkot", "Tal Barak", "Tal Kabesa", "Tal Mashiah", "vlad", "Yael Fisher", "Yael Shenker", "Yanir Shaked", "Yoad Gantz"];
+let gStudentNames = ["Alina Boshkov", "Alon Tal", "Arkadi Zvalinov", "Ben Levi", "Ben Yager", "Coral Solomon", "Daniil Glezer", "Edgar", "Eyal Ganor", "Gilad Bergmann", "Igor", "Ilya Levin", "Lior BenHaim", "Mai Aloni", "Margad T", "Matt Gordon", "Mor Zeevi", "Nevo Kotlovsky", "Niv Leibovitch", "Ohad Avidar", "Paolo Groppi", "Roy Amar", "Shaked Katsir", "shay rosenthal", "Shmuel Elkis", "Tal Azenkot", "Tal Kabesa", "Tal Mashiah", "vlad", "Yael Fisher", "Yael Shenker", "Yanir Shaked", "Yoad Gantz"];
 let gDesks = null;
 let gIsLoading = false;
 let gMissingDesks = [];
